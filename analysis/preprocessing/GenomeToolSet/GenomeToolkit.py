050d0192ff5337d17b1b7575c5eb9db20051202d
Ioannakiou <ioannakiourth@gmail.com>
2026-02-16 16:56:00 +0000

    feat(genomeToolkit): enhance k-mer functionality with detailed documentation and add most frequent k-mers method

--- analysis/preprocessing/GenomeToolSet/GenomeToolkit.py
+++ analysis/preprocessing/GenomeToolSet/GenomeToolkit.py
@@ -5,7 +5,17 @@ class genomeToolkit:
 
     def count_kmer(self, sequence, kmer):
         # sourcery skip: inline-immediately-returned-variable, sum-comprehension
-        """Counts repeating k-mers in a given sequence. Includes overlapping k-mers."""
+        """
+        Counts the number of times a specific k-mer appears in a given sequence, 
+        including overlapping k-mers.
+        
+        Parameters:
+            sequence (str): The DNA sequence to search in.
+            kmer (str): The specific k-mer to search for in the sequence.
+            
+        Returns: 
+            int: The number of times the k-mer appears in the sequence.
+        """
         kmer_count = 0
         for position in range(len(sequence) - len(kmer) -1):
             print(sequence[position:position + len(kmer)], "=", kmer)
@@ -13,6 +23,32 @@ class genomeToolkit:
                 kmer_count += 1
         return kmer_count
     
+    def find_most_frequent_kmers(self, sequence, k_len):
+        """
+        Finds the most frequent k-mers of a given length in a DNAstring
+        
+        Parameters:
+            sequence (str): The DNA string to search.
+            k_len (int): The length of the k-mers to search for.
+            
+        Returns:
+            list: A list of the most frequent k-mers in the DNA string.
+        """
+        # Initialize a dictionary to store k-mer frequencies
+        kmer_frequencies = {}
+        
+        # Loop through the DNA string and count k-mer frequencies
+        for position in range(len(sequence) - k_len + 1):
+            kmer = sequence[position:position + k_len]
+            if kmer in kmer_frequencies:
+                kmer_frequencies[kmer] += 1
+            else:
+                kmer_frequencies[kmer] = 1    
+        # Find the highest frequency of any k-mer
+        highest_frequency = max(kmer_frequencies.values())
+        
+        return [kmer for kmer, frequency in kmer_frequencies.items() if frequency == highest_frequency]
+
     
 """
 seq = AATGCGAAAC | Legnth: 10
--- analysis/preprocessing/GenomeToolSet/application.py
+++ analysis/preprocessing/GenomeToolSet/application.py
@@ -2,9 +2,11 @@ from GenomeToolkit import genomeToolkit
 
 gt = genomeToolkit()
 
-seq = "AAATGCGTACGTAGCTAAAGCTAAGCTAAAGCTAGCTA"
+seq = "AAATGCAAA"
 kmer = "AAA"
+k_len = 3
 
 print(f"Sequence: {seq}")
 print(f"K-mer: {kmer}")
-print(f"Repeats found: {gt.count_kmer(seq, kmer)}")
+# print(f"Repeats found: {gt.count_kmer(seq, kmer)}")
+print(f"Most frequent k-mers: {gt.find_most_frequent_kmers(seq, k_len)}")
